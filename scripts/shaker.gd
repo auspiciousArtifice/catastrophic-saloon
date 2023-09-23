d43c02460bf9f8ff658e19f73cd96b224f6382e6
auspiciousArtifice <harrywang703@gmail.com>
2023-09-23 01:15:25 -0400

    Makes walls bouncy and renames script for shaker node

--- scripts/shaker.gd
+++ scripts/shaker.gd
@@ -6,12 +6,11 @@ func _process(delta):
 
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		print("clicked in object")
+#		print("clicked in object")
 		var mouse_pos = get_global_mouse_position()
 		var force_offset = -(get_global_position() - mouse_pos)
 		var force_bullet_x = get_global_position().x - mouse_pos.x
 		var force_bullet_y = -1500
 		var force_bullet = Vector2(force_bullet_x * abs(force_bullet_x) / 3, force_bullet_y)
-#		apply_central_impulse(force_bullet)
 		apply_impulse(force_bullet, force_offset)
-		print("Bullet Force Vector: %s" % force_bullet)
+#		print("Bullet Force Vector: %s" % force_bullet)
